a6180fbddc5b256ff43903ad1f35cc2ef3f933e3
Christopher <easonmu@hotmail.com>
2017-12-14 00:17:32 +0800

    file save

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <p>{{msg}}</p>
-    <input v-model="title" placeholder="title">
-    <input v-model="content" placeholder="content">
-    <button @click="setTodo">Set Todo</button>
-    <div>ID: {{todoId}}</div>
-    <input v-model="name" placeholder="name">
-    <input v-model="priority" placeholder="priority">
-    <button @click="setTodoFolder">Set TodoFolder</button>
+    <div>
+      <input v-model="title" placeholder="title">
+      <input v-model="content" placeholder="content">
+      <button @click="setTodo">Set Todo</button>
+    </div>
+    <div>
+      <input v-model="name" placeholder="name">
+      <input v-model="priority" placeholder="priority">
+      <button @click="setTodoFolder">Set TodoFolder</button>
+    </div>
+    <div>
+      <button @click="uploadFile">upload file</button>
+    </div>
+    <div>
+      <input type="file" id="photoFileUpload"/><button @click="uploadLocalFile">upload local file</button>
+    </div>
+    
+    <!-- <div>ID: {{todoId}}</div> -->
+    
 
   </div>
 </template>
@@ -51,23 +63,54 @@ export default {
     },
     setTodoFolder () {
       // // 设置名称
-      // todoFolder.set('name', this.name)
-      // // 设置优先级
-      // todoFolder.set('priority', this.priority)
-      // todoFolder.save().then(function (todo) {
-      //   console.log('objectId is ' + todo.id)
-      // }, function (error) {
-      //   console.error(error)
-      // })
-       AV.Query.doCloudQuery('insert into TodoFolder(name, priority) values("工作", "1")').then(function (data) {
-        // data 中的 results 是本次查询返回的结果，AV.Object 实例列表
-        var results = data.results;
-        console.log(data)
+      todoFolder.set('name', this.name)
+      // 设置优先级
+      todoFolder.set('priority', this.priority)
+      todoFolder.save().then(function (todo) {
+        console.log('objectId is ' + todo.id)
       }, function (error) {
-        //查询失败，查看 error
-        console.error(error);
+        console.error(error)
       })
+      //  AV.Query.doCloudQuery('insert into TodoFolder(name, priority) values("工作", "1")').then(function (data) {
+      //   // data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+      //   var results = data.results;
+      //   console.log(data)
+      // }, function (error) {
+      //   //查询失败，查看 error
+      //   console.error(error);
+      // })
+    },
+    uploadFile () {
+      // 数据流
+      var data = { base64: '6K+077yM5L2g5Li65LuA5LmI6KaB56C06Kej5oiR77yf' }
+      var file = new AV.File('resume.txt', data)
+      file.save().then(function (file) {
+        console.log('3333')
+        console.log(file)
+      }, function (error) {
+        console.error(error)
+      })
+      // var bytes = [0xBE, 0xEF, 0xCA, 0xFE]
+      // var byteArrayFile = new AV.File('myfile.txt', bytes)
+      // byteArrayFile.save()
+    },
+    uploadLocalFile () {
+      var fileUploadControl = document.getElementById('photoFileUpload')
+      if (fileUploadControl.files.length > 0) {
+        var localFile = fileUploadControl.files[0]
+        var name = '傻狗狗.jpg'
+        var file = new AV.File(name, localFile)
+        file.save().then(function (file) {
+          // 文件保存成功
+          console.log(file)
+          console.log(file.url())
+        }, function (error) {
+          // 异常处理
+          console.error(error)
+        })
+      }
     }
+
   },
   created () {
     // console.log('3333')
@@ -90,5 +133,8 @@ li {
 }
 a {
   color: #42b983;
+}
+div {
+  margin-top: 5px;
 }
 </style>
